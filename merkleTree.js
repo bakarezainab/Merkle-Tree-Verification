db670b42873f4541d98a2e6b4267b6f235706559
unknown <bakarezainab74@gmail.com>
2025-01-27 00:41:07 +0000

    commit2

--- merkleTree.js
+++ merkleTree.js
@@ -11,3 +11,8 @@ const proof = tree.getProof(leaf)
 console.log(tree.verify(proof, leaf, root)) // true
 
 
+const badLeaves = ['a', 'x', 'c'].map(x => SHA256(x))
+const badTree = new MerkleTree(badLeaves, SHA256)
+const badLeaf = SHA256('x')
+const badProof = badTree.getProof(badLeaf)
+console.log(badTree.verify(badProof, badLeaf, root)) // false
